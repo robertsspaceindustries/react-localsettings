dd192f10d3a62c402c0f403b2dca9a0774551d3a
Astrid <github@astrid.exposed>
2023-09-18 00:32:15 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, SetStateAction } from "react";
+
+/**
+ * @param {string} name The name of this setting
+ * @param {any} defaultValue The default value for this setting
+ * @param {string} localStorageItem The name of the localStorage item, settings are saved in here
+ * @returns {[any, SetStateAction]}
+ */
+export default function useLocalStorage(name, defaultValue, localStorageItem) {
+	localStorageItem = localStorageItem ?? "react-localsettings";
+
+	const [state, setState] = useState(defaultValue ?? "");
+
+	useEffect(() => {
+		const storedSettings = JSON.parse(localStorage.getItem(localStorageItem) ?? "{}");
+		if (storedSettings[name]) {
+			setState(storedSettings[name]);
+		}
+	});
+
+	/**
+	 * @param {any} value
+	 */
+	function newSetState(value) {
+		setState(value);
+
+		let storedSettings = JSON.parse(localStorage.getItem(localStorageItem) ?? "{}");
+		storedSettings[name] = value;
+		localStorage.setItem(JSON.stringify(storedSettings));
+	}
+
+	return [state, newSetState];
+}
